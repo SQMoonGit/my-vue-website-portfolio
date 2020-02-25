964a3c90dee0751bcdb587d4168b65e948bb0624
SQMoonGit <hykmoon@gmail.com>
2020-02-24 21:03:14 -0600

    Added some templates

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,83 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <v-app id="inspire">
+      <v-navigation-drawer :clipped="clipped" v-model="sideMenuExpansion" app disable-resize-watcher class="yellow accent-1">
+        <v-list>
+          <v-list-item link v-for="links in changeList" :keys="link">
+            {{links}}
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-app-bar fixed app :clipped-left="clipped" color="blue darken-3">
+        <v-app-bar-nav-icon @click.stop="sideMenuExpansion =!sideMenuExpansion"></v-app-bar-nav-icon>
+        <v-btn rounded text>
+          <router-link class="title white--text" :to="{name: 'home'}">Home</router-link>
+        </v-btn>
+        <v-btn rounded text>
+          <router-link class="white--text" :to="{name: 'MHW Builder'}">MHW Builder</router-link>
+        </v-btn>
+        <v-btn rounded text>
+          <router-link class="white--text" :to="{name: 'Twitch Stats'}">Twitch Stats</router-link>
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-autocomplete
+            hide-no-data hide-selected hide-details
+            v-model="model" :loading="isLoading" :items="items"
+            clearable class="white--text col-2" color="white"
+            placeholder="Type to Search" append-icon="fa-search">
+        </v-autocomplete>
+
+      </v-app-bar>
+
+      <v-content>
+        <v-container fluid class="pa-0">
+          <router-view/>
+        </v-container>
+      </v-content>
+    </v-app>
+
+    <v-btn fab bottom right fixed id="scroll-button" @click="scrollToTop">
+      <v-icon>fa-arrow-up</v-icon>
+    </v-btn>
+
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from './components/HelloWorld.vue'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 
-@Component({
-  components: {
-    HelloWorld
+@Component({})
+export default class App extends Vue {
+
+  private isLoading?: boolean = false;
+  private model!: any;
+  private items!: any;
+
+  private currentPage?: string = 'home';
+  private sideMenuExpansion: boolean = true;
+  private clipped: boolean = false;
+
+  // get changeList(){
+  //
+  // }
+
+  public scrollToTop(){
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    })
   }
-})
-export default class App extends Vue {}
+  //
+  // @Watch('$route.path', {deep:true, immediate: true})
+  // public changeCurrentPage(){
+  //   if(this.$route.path.includes())
+  // }
+
+}
 </script>
 
 <style lang="scss">
@@ -26,4 +89,11 @@ export default class App extends Vue {}
   color: #2c3e50;
   margin-top: 60px;
 }
+
+#scroll-button{
+  background-color: #F9A825;
+  color: #DCE775;
+}
+
+
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="homepage">
+    Home Page text
+
+    <p>Add some cards and fancy stuff here</p>  
+
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Watch} from 'vue-property-decorator';
+
+@Component({})
+export default class HomeView extends Vue{
+}
+</script>
+
+<style lang="scss">
+</style>
