7f9f4959118ccfc0aa65d0d609a4230b7459c544
SQMoonGit <hykmoon@gmail.com>
2020-03-17 19:23:48 -0500

    icons and api structure change

--- src/components/EquipComponent.vue
+++ src/components/EquipComponent.vue
@@ -1,15 +1,33 @@
 <template>
     <div>
         <v-card>
-            <v-row>
+            <v-row no-gutters>
                 <v-col>
-                    <!-- <v-autocomplete 
-                        hide-details hide-selected hide-no-data 
-                        v-model="" :loading="isLoading">
-                    </v-autocomplete> -->
+                    <v-autocomplete 
+                        hide-details hide-selected hide-no-data return-object
+                        :items="weaponsList" item-text="name"
+                        :prepend-inner-icon="'mhw-' + weaponChosen.type"
+                        v-model="weaponChosen" :loading="isLoading">
+                    </v-autocomplete>
 
-                    <!-- <v-autocomplete >
-                    </v-autocomplete> -->
+                    <v-autocomplete
+                        v-if="!isArmorSet"
+                        v-for="(armorType, index) in armorPieces"
+                        hide-details hide-selected hide-no-data return-object
+                        :items="armorList.filter((v,i) => {return v.type === armorType})" item-text="name"
+                        :prepend-inner-icon="'mhw-' + armorType"
+                        v-model="armorSelected[armorType]" :loading="isLoading">
+                    </v-autocomplete>
+
+                    <v-autocomplete
+                        v-if="isArmorSet"
+                        hide-details hide-selected hide-no-data return-object
+                        :items="armorSets" item-text="name"
+                        :prepend-inner-icon="'mhw-armorset'"
+                        v-model="armorSetSelected" :loading="isLoading" >
+                    </v-autocomplete>
+
+                    <v-btn dense @click="updateArmorSelected">Armorset</v-btn>
 
                 </v-col>
 
@@ -27,17 +45,32 @@
 import {Vue, Component, Prop} from 'vue-property-decorator';
 import {Weapons} from "@/model/weapons";
 import {Armor} from "@/model/armor";
+import {Armorsets} from "@/model/armorsets";
 
 @Component
 export default class EquipComponent extends Vue{
     @Prop() mhwLink!: string;
 
     private isLoading: boolean = false;
-    private model: string = "";
+    private isArmorSet: boolean = false;
+
     private armorList: Armor[] = [];
+    private armorSelected: object = {};
+    private armorPieces: string[] = [];
+
+    private armorSets: Armorsets[] = [];
+    private armorSetSelected: Armorsets = new Armorsets();
+
     private weaponsList: Weapons[] = [];
+    private weaponChosen: Weapons = new Weapons();
 
     created(){
+        this.armorPieces = ['head', 'chest', 'gloves', 'waist', 'legs'];
+        this.armorPieces.forEach((value: string) =>{
+            this.$set(this.armorSelected, value, '');
+        })
+
+        //API call for armor
         this.$http.get(`${this.mhwLink}/armor`).then((result) =>{
             if(result.ok && result.data){
                 this.armorList = result.data;
@@ -49,6 +82,7 @@ export default class EquipComponent extends Vue{
             console.log("[ERROR] - GET/armor");
         })
 
+        //API call for weapons
         this.$http.get(`${this.mhwLink}/weapons`).then((result) =>{
             if(result.ok && result.data){
                 this.weaponsList = result.data;
@@ -59,10 +93,33 @@ export default class EquipComponent extends Vue{
             alert(error.body.error);
             console.log("[ERROR] - GET/weapons");
         })
+
+        //API call for armorsets
+        this.$http.get(`${this.mhwLink}/armor/sets`).then((result) =>{
+            if(result.ok && result.data){
+                this.armorSets = result.data;
+            } else{
+                throw new Error(result.statusText);
+            }
+        }, (error) =>{
+            alert(error.body.error);
+            console.log("[ERROR] - GET/armor/sets");
+        })
+    }
+
+    public updateArmorSelected(){
+        this.isArmorSet = !this.isArmorSet;
     }
 }
 </script>
 
 <style lang="scss">
+    div{
+        .v-autocomplete{
+            padding-top: 0;
+            margin-top: 1px;
+            margin-bottom: 5px;
+        }
+    }
 
 </style>
--- src/views/MHWView.vue
+++ src/views/MHWView.vue
@@ -1,28 +1,89 @@
 <template>
   <div>
-    <v-row>
+    <v-row no-gutters>
       <v-col>
-        <v-card>
-          Armor and Weapon card selection here
-        </v-card>
+        <equip-component ></equip-component>
       </v-col>
 
       <v-col>
-        <v-card>
-          Monster Selection here
-        </v-card>
+        <monsters-component ></monsters-component>
       </v-col>
-
     </v-row>
+
+    <v-card>
+      Some information and description and stats and stuff here
+    </v-card>
 
   </div>
 </template>
 
 <script lang="ts">
 import {Component, Vue, Watch} from 'vue-property-decorator';
+import EquipComponent from '../components/EquipComponent.vue';
+import MonstersComponent from '../components/MonstersComponent.vue';
 
-@Component({})
+@Component({
+  components: {
+    MonstersComponent, EquipComponent
+  }
+})
 export default class MHWView extends Vue{
+  private isLoading?: boolean = false;
+  private model: string = "";
+  private mhwURL: string = "https://mhw-db.com";
+
+  created(){
+    //API call for armor
+    this.$http.get(`${this.mhwURL}/armor`).then((result) =>{
+        if(result.ok && result.data){
+            this.armorList = result.data;
+        } else{
+            throw new Error(result.statusText);
+        }
+    }, (error) =>{
+        alert(error.body.error);
+        console.log("[ERROR] - GET/armor");
+    })
+
+    //API call for weapons
+    this.$http.get(`${this.mhwURL}/weapons`).then((result) =>{
+        if(result.ok && result.data){
+            this.weaponsList = result.data;
+        } else{
+            throw new Error(result.statusText);
+        }
+    }, (error) =>{
+        alert(error.body.error);
+        console.log("[ERROR] - GET/weapons");
+    })
+
+    //API call for armorsets
+    this.$http.get(`${this.mhwURL}/armor/sets`).then((result) =>{
+        if(result.ok && result.data){
+            this.armorSets = result.data;
+        } else{
+            throw new Error(result.statusText);
+        }
+    }, (error) =>{
+        alert(error.body.error);
+        console.log("[ERROR] - GET/armor/sets");
+    })
+
+    //API call for monsters
+    this.$http.get(`${this.mhwURL}/monsters`).then((result) =>{
+        if(result.ok && result.data){
+            this.monsterList = result.data;
+            
+        } else{
+            throw new Error(result.statusText);
+        }
+    }, (error) =>{
+        alert(error.body.error);
+        console.log("[ERROR] - GET/monsters");
+    })
+  }
+
+
 }
 </script>
 
