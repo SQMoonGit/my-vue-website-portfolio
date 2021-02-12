9c5b7e831513a74b37916e053697749299d63d13
SQMoonGit <hykmoon@gmail.com>
2021-02-12 16:37:06 -0600

    Replaced vue-resource with vue-axios

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
-import VueResource from "vue-resource";
+import axios from "axios";
+import VueAxious from "vue-axios";
 // import VueGoogleCharts from "vue-google-charts";
 
 // Vue.use(VueGoogleCharts);
-Vue.use(VueResource);
+Vue.use(VueAxious, axios);
 Vue.config.productionTip = false;
 
 new Vue({
