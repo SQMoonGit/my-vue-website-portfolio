3176ab1a46b7ef8400d698bc8639126c51e36cda
SQMoonGit <hykmoon@gmail.com>
2021-03-10 21:22:11 -0600

    More optimizing

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,30 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import HomeView from './views/HomeView.vue';
-import MHWView from './views/MHWView.vue';
-import TwitchView from './views/TwitchView.vue';
-
+import Vue from "vue";
+import Router from "vue-router";
+import HomeView from "./views/HomeView.vue";
 
 Vue.use(Router);
 
 export default new Router({
-  mode:'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
-      props: true,
+      props: true
     },
     {
-      path: '/mhw_builder',
-      name: 'MHW Builder',
-      component: MHWView,
-      props: true,
+      path: "/mhw_builder",
+      name: "MHW Builder",
+      component: () => import("./views/MHWView.vue"),
+      props: true
     },
     {
-      path: '/twitch_stats',
-      name: 'Twitch Stats',
-      component: TwitchView,
-      props: true,
+      path: "/twitch_stats",
+      name: "Twitch Stats",
+      component: () => import("./views/TwitchView.vue"),
+      props: true
     }
   ]
-})
+});
